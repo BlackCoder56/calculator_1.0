e54470138870e4efc3cfe1399b211a5b2a18b786
BlackCoder56 <blackcoda56@gmail.com>
2025-04-02 13:51:14 +0300

    style function to add bg-color to frame

--- view.py
+++ view.py
@@ -2,7 +2,7 @@ from tkinter import *
 from tkinter import ttk
 
 root = Tk()
-
+root.title("CALCULATOR 1.0")
 
 # Get the screen width and height
 screen_width = root.winfo_screenwidth()
@@ -22,8 +22,14 @@ root.geometry(f'{window_width}x{window_height}+{position_right}+{position_top}')
 # Disable resizing
 root.resizable(False, False)
 
+# Create a style for the ttk.Frame
+style = ttk.Style()
+style.configure("TFrame", background="lightblue")
+
 # Frame to hold ttk components
-frame = ttk.Frame(root)
+frame = ttk.Frame(root, style="TFrame", width=window_width, height=window_height)
+frame.pack_propagate(False)
+# frame.pack()
 frame.grid(column=0, row=0)
 
 # Run the Tkinter event loop
