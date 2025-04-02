6461a65dedfda4519af82d3eb681c1046bbe435a
BlackCoder56 <blackcoda56@gmail.com>
2025-04-02 12:42:54 +0300

    Centering TK window on screen

---
+++ view.py
@@ -0,0 +1,27 @@
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+
+
+# Get the screen width and height
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+# Set the window size
+window_width = 400
+window_height = 300
+
+# Calculate the position to center the window
+position_top = (screen_height // 2) - (window_height // 2)
+position_right = (screen_width // 2) - (window_width // 2)
+
+# Set the window size and position
+root.geometry(f'{window_width}x{window_height}+{position_right}+{position_top}')
+
+
+frame = ttk.Frame(root)
+frame.grid(column=0, row=0)
+
+
+root.mainloop()
