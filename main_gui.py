84fc1c0cc34aee49bbe964c29accb4c82b6b32d3
BlackCoder56 <blackcoda56@gmail.com>
2025-04-04 13:12:06 +0300

    Entry widget to get variables for calculations

--- main_gui.py
+++ main_gui.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import font
 from tkinter import ttk
 
 root = Tk()
@@ -9,7 +10,7 @@ screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
 
 # Set the window size
-window_width = 400
+window_width = 410
 window_height = 600
 
 # Calculate the position to center the window
@@ -30,14 +31,17 @@ style.configure("TFrame", background="#0000")
 frame = ttk.Frame(root, style="TFrame", width=window_width, height=window_height)
 frame.pack_propagate(False)
 # frame.pack()
-frame.grid(column=0, row=0)
+frame.grid(column=0, row=0, padx=2, pady=5)
 
 # Gui
 listvalue = ('example1', 'example2', 'example3')
 lname = StringVar(value=listvalue)
-historyList = Listbox(frame, listvariable=lname, height=3, width=50)
+historyList = Listbox(frame, listvariable=lname, bd=2, relief="solid", highlightbackground="red", height=5, width=50)
 historyList.grid(column=0, row=0, columnspan=4, sticky=(N,S,E,W))
 
+textsize = font.Font(size=22, weight='bold')
+calculation_entry = ttk.Entry(frame, font=textsize,justify=RIGHT)
+calculation_entry.grid(column=0, row=1, columnspan=4, sticky=(N,S,E,W))
 
 
 
